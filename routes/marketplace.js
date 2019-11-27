b2c38734044722a665701ec0ef24a1fb1d2ff9de
Osama Adam <Osamaadam98@gmail.com>
2019-11-27 19:32:22 +0200

    added delete and update requests

--- routes/marketplace.js
+++ routes/marketplace.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 let Market = require("../models/marketplace.model");
 
-router.get("/marketplace", (req, res) => {
+router.get("/", (req, res) => {
 	Market.find()
 		.then((items) => res.json(items))
 		.catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
-router.post("/marketplace/add", (req, res) => {
+router.post("/add", (req, res) => {
 	const img = req.body.img;
 	const title = req.body.title;
 	const name = req.body.name;
@@ -23,6 +23,28 @@ router.post("/marketplace/add", (req, res) => {
 	newItem
 		.save()
 		.then(() => res.json("item saved"))
+		.catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+router.delete("/delete/:id", (req, res) => {
+	Market.findByIdAndDelete(req.params.id)
+		.then(() => res.json(`item deleted`))
+		.catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+router.post("/update/:id", (req, res) => {
+	Market.findById(req.params.id)
+		.then((item) => {
+			item.img = req.body.img;
+			item.title = req.body.title;
+			item.name = req.body.name;
+			item.desc = req.body.desc;
+
+			item
+				.save()
+				.then(() => res.json(`item updated.`))
+				.catch((err) => res.status(400).json(`Error: ${err}`));
+		})
 		.catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
