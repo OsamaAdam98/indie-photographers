24b10155b4c413c54227055b61dc9a72baaea5b1
Osama Adam <osamaadam98@gmail.com>
2020-01-30 15:19:18 +0200

    small visual refinement

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,14 +1,11 @@
 import React from "react";
 import "../css/home.css";
+import homeCover from "../assets/home-cover.jpg";
 
 export default function Home() {
 	return (
 		<div className="container">
-			<img
-				className="main-img"
-				alt="Home cover"
-				src="https://images.squarespace-cdn.com/content/v1/56cc470427d4bdfc954a8759/1569575406292-BNP3LT438HH1LWXISS07/ke17ZwdGBToddI8pDm48kJRqFJ19D4P4EwsC9z3fiewUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYy7Mythp_T-mtop-vrsUOmeInPi9iDjx9w8K4ZfjXt2dkV64dCjSK7Zaaf7dwPYPO_gHf_vjqrS5WJoq1nmwotrP7cJNZlDXbgJNE9ef52e8w/main_camera_lenses.jpg?format=1000w"
-			/>
+			<img className="main-img" alt="Home cover" src={homeCover} />
 			<h1 className="main-line">
 				<span className="highlight">In development</span>
 			</h1>
